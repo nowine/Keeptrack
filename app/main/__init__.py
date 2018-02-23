9b691767959dbbdccdbe9824593a65a4d1fbc842
nil <nowine@163.com>
2018-02-24 01:25:54 +0800

    Add new component

---
+++ app/main/__init__.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+
+from flask import Blueprint
+
+main = Blueprint('main', __name__,
+        static_folder='../static',
+        template_folder='../templates/main')
+
+from . import views
