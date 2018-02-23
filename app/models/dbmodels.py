b5aba2a921c5f1ee0337a1d233cf65ce854fc88d
nil <nowine@163.com>
2018-02-24 00:37:52 +0800

    For Pushing to Github

--- app/models/dbmodels.py
+++ app/models/dbmodels.py
@@ -4,6 +4,8 @@ class Funds(db.Model):
     __tablename__ = 'funds'
     id = db.Column(db.Integer, primary_key=True)
     code = db.Column(db.String(10), index=True) 
+    quote = db.Column(db.Float)
+    quote_date = db.Column(db.Date)
 
     def to_dict(self):
         return {
